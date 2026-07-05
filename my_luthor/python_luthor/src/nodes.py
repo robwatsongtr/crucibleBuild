25eb9b786dec8139d7b12113092a71515360c45b
Rob Watson <rwatso@gmail.com>
2026-07-04 19:32:51 -0700

    system prompt to not give code for syntax questions

---
+++ my_luthor/python_luthor/src/nodes.py
@@ -0,0 +1,10 @@
+
+class NumberNode:
+    def __init__(self, number):
+        self.number = number 
+
+    
+
+class IdentifierNode:
+    def __init__(self, identifier):
+        self.identifier = identifier
